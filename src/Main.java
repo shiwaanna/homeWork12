ff908b852aa403c3569f284c6c3858bd596dba15
anna <shiwa.ann@gmail.com>
2023-06-10 20:42:48 +0400

    Выполнено ДЗ 12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author tolstoy = new Author("Lev", "Tolstoy");
+        Book warAndPeace = new Book("War and Peace", tolstoy, 1835);
+        System.out.println("Книга: " + warAndPeace.getTitle() + " автор: " + warAndPeace.getAuthor().getName() + " " + warAndPeace.getAuthor().getLastName());
+
+        warAndPeace.setPublicationYear(1859);
+        System.out.println(warAndPeace.getPublicationYear());
+
+        Author dostoevsky = new Author("Fyodor", "Dostoevsky");
+        Book crimeAndPunishment = new Book("Crime and Punishment", dostoevsky, 1866);
+        System.out.println("Книга: " + crimeAndPunishment.getTitle() + " автор: " + crimeAndPunishment.getAuthor().getName() + " " + crimeAndPunishment.getAuthor().getLastName());
     }
 }
